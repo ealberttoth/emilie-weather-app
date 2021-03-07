85d1381a04f2102312132e757b83178be65a5a96
ealberttoth <77371769+ealberttoth@users.noreply.github.com>
2021-03-07 18:02:10 -0500

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -27,23 +27,23 @@ let today = new Date();
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = formatDate(today);
 
-
 function showTemperature(response){
   let temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#current-temp");
   let feelsLike = Math.round(response.data.main.feels_like);
   let currentFeelsLike = document.querySelector("#current-feels-like");
   let currentDescription = document.querySelector("#current-description");
-  let currentPercipitation = document.querySelector("#percipitation");
   let currentHumidity = document.querySelector("#humidity");
   let currentWind = document.querySelector("#wind");
+  let currentIcon = document.querySelector("#icon");
  
   currentTemp.innerHTML = (temperature);
   currentFeelsLike.innerHTML = (`Feels like ${feelsLike} °C`);
   currentDescription.innerHTML = response.data.weather[0].description;
-  currentPercipitation.innerHTML = response.data.main.percipitation;
   currentHumidity.innerHTML = response.data.main.humidity;
   currentWind.innerHTML = Math.round(response.data.wind.speed);
+  currentIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  currentIcon.setAttribute("alt", response.data.weather[0].description);
 }
   
 
@@ -51,10 +51,11 @@ function searchCity(event){
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   let h2 = document.querySelector("h2");
-  h2.innerHTML = (cityInput.value);
   let units = "metric";
   let apiKey = "28c3ae709e1f96587f82b0797ade3e71";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=${units}`;
+  
+  h2.innerHTML = (cityInput.value);
   axios.get(apiUrl).then(showTemperature);
 }
 
@@ -64,12 +65,13 @@ searchForm.addEventListener("submit", searchCity);
 function showCurrentTemperature(response) {
   let city = response.data.name;
   let h2 = document.querySelector("h2");
-  h2.innerHTML = (city);
   let temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector("#current-temp");
-  currentTemp.innerHTML = (temperature);
   let feelsLike = Math.round(response.data.main.feels_like);
   let currentFeelsLike = document.querySelector("#current-feels-like");
+  
+  h2.innerHTML = (city);
+  currentTemp.innerHTML = (temperature);
   currentFeelsLike.innerHTML = (`Feels like ${feelsLike} °C`);
 }
 
